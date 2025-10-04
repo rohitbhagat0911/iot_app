c23f312f1e9c3cc1c5edc58b9124fc9193df2f9a
OnDev-Webs <roshansahani535@gmail.com>
2025-10-04 19:00:41 +0530

    fix: conn backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -6,8 +8,7 @@ void main() {
 
 // stateless static themes
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); // widget can be function or class
-  // stateless and stateless widget are thre
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +23,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// stateful 
-
+// stateful
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -35,11 +35,33 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String _statusMessage = 'Press the cloud button to check connectivity';
 
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
+  }
+
+  Future<void> _checkBackend() async {
+    try {
+      final uri = Uri.parse('http://localhost:3000/connect');
+      final response = await http.get(uri);
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        setState(() {
+          _statusMessage = data['message'];
+        });
+      } else {
+        setState(() {
+          _statusMessage = 'Error: ${response.statusCode}';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _statusMessage = 'Exception: $e';
+      });
+    }
   }
 
   @override
@@ -49,7 +71,8 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -60,13 +83,32 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            const SizedBox(height: 32),
+            Text(
+              _statusMessage,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            heroTag: 'counter',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: _checkBackend,
+            tooltip: 'Check Backend',
+            heroTag: 'connect',
+            child: const Icon(Icons.cloud),
+          ),
+        ],
       ),
     );
   }
